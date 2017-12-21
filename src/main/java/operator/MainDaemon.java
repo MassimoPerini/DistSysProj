a87a0152f9805dcfa38dcd47d967edf406ab178b
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2017-12-21 21:19:48 +0100

    adding some packages

---
+++ src/main/java/operator/MainDaemon.java
@@ -0,0 +1,7 @@
+package operator;
+
+/**
+ * Created by higla on 21/12/2017.
+ */
+public class MainDaemon {
+}
--- src/main/java/supervisor/MainSupervisor.java
+++ src/main/java/supervisor/MainSupervisor.java
@@ -1,7 +1,9 @@
+package supervisor;
+
 /**
  * Created by massimo on 21/12/17.
  */
-public class Main {
+public class MainSupervisor {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
